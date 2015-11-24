045add56c16aa2b3633a3ee3f2a9cb18124609b2
Bob Ma <mjc0608@gmail.com>
2015-11-25 05:46:39 +0800

    add download

---
+++ download.sh
@@ -0,0 +1,3 @@
+wget https://storage.googleapis.com/golang/go1.5.1.linux-amd64.tar.gz
+curl -L  https://github.com/coreos/etcd/releases/download/v2.2.2/etcd-v2.2.2-linux-amd64.tar.gz -o etcd-v2.2.2-linux-amd64.tar.gz
+git clone https://github.com/caasjtu/kubernetes.git
